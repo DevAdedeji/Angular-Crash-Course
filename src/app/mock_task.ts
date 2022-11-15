29a3d8f1164784c35673aa8e15eb7a89cdef6d3c
DevAdedeji <adedejitewogbade2@gmail.com>
2022-11-15 16:52:36 +0100

    Learnt about httpClient

--- src/app/mock_task.ts
+++ src/app/mock_task.ts
@@ -18,4 +18,10 @@ export const TASKS: Task[] = [
     day: 'May 7th at 12:30pm',
     reminder: false,
   },
+  {
+    id: 4,
+    text: 'Test',
+    day: 'May 7th at 12:30pm',
+    reminder: false,
+  }
 ]
